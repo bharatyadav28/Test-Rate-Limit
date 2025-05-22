0c29f67118340540c0af957362432d2079dd3dda
Bharat Yadav <yadavbharat386@gmail.com>
2025-05-22 16:44:31 +0530

    Add input field for ip

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,21 @@ import { useState } from "react";
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState("");
+  const [customIp, setCustomIp] = useState("");
 
   const handleReq = async () => {
     if (isLoading) return;
     try {
       setIsLoading(true);
-      const response = await fetch(
-        "https://string-geo-backend-tbgf.onrender.com/api/test"
-      );
+      // const url = "https://string-geo-backend-tbgf.onrender.com";
+      const url = "http://localhost:4000";
+      const response = await fetch(`${url}/api/test`, {
+        method: "POST",
+        body: JSON.stringify({ customIp: customIp?.trim() }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
       const res = await response.json();
       if (!response.ok) {
         throw Error(res.message || "Something went wrong");
@@ -29,7 +36,17 @@ export default function Home() {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-4">
       <div className="text-white text-lg flex break-all">{data}</div>
-      <div>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          value={customIp}
+          onChange={(e) => {
+            const value = e.target.value;
+            setCustomIp(value);
+          }}
+          placeholder="Enter any custom ip"
+          className="text-white px-2"
+        />
         <button
           onClick={handleReq}
           className="p-4  my-auto bg-amber-800 cursor-pointer rounded -sm"
